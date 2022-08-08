3b8ad86923395e5cef7fd1e4ccc5c9bbb01b0172
DARJIDASHRATH <110831773+DARJIDASHRATH@users.noreply.github.com>
2022-08-08 19:31:40 +0530

    Add files via upload

--- src/basicofjava/Wrappers.java
+++ src/basicofjava/Wrappers.java
@@ -11,9 +11,7 @@ public class Wrappers {
 
 	@SuppressWarnings("removal")
 	public static void main(String[] args) {
-
 		int i = 100;
-
 		Integer j = new Integer(i); // boxing
 		System.out.println(i == j);
 	}

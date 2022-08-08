9a3f9c54b29d0eb3c285a84a6183ec0343040d52
DARJIDASHRATH <110831773+DARJIDASHRATH@users.noreply.github.com>
2022-08-08 19:42:56 +0530

    Add files via upload

--- src/loops/ContinueStatement.java
+++ src/loops/ContinueStatement.java
@@ -1,7 +1,7 @@
 package loops;
 
 /**
- * here we use continue statment in for loop.
+ * Here we use continue statment in for loop.
  * 
  * @author DASHRATH
  * @version 6.2
@@ -11,10 +11,10 @@ public class ContinueStatement {
 
 	public static void main(String[] args) {
 		int i = 1;
-		for (i = 1; i <= 10; i++) // for loop conditions. loop go up to 10.
+		for (i = 1; i <= 10; i++) // For loop conditions. loop go up to 10.
 		{
 			if (i == 7) {
-				continue; // here we skip number 7 by using continue statment.
+				continue; // Here we skip number 7 by using continue statment.
 			}
 			System.out.println(i);
 

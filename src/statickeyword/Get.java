e56652a3e75b66575fe4057cb1e77e98cd2cfa69
DARJIDASHRATH <110831773+DARJIDASHRATH@users.noreply.github.com>
2022-08-08 19:54:34 +0530

    Add files via upload

--- src/statickeyword/Get.java
+++ src/statickeyword/Get.java
@@ -1,7 +1,7 @@
 package statickeyword;
 
 /**
- * using static keyword we count buss pass through get
+ * Using static keyword we count buss pass through get
  *
  * @author DASHRATH
  * @version 12.0
@@ -12,15 +12,15 @@ public class Get {
 	static int count = 0;
 
 	Get() {
-		count++; // increamenting
-		System.out.println(count); // printing count value
+		count++; // Increamenting
+		System.out.println(count); // Printing count value
 	}
 
 	public static void main(String[] args) {
 		{
 			System.out.println("Bus Passing Through Get");
 		}
-		// craeting an object
+		// Craeting an object
 		Get b1 = new Get();
 		Get b2 = new Get();
 		Get b3 = new Get();

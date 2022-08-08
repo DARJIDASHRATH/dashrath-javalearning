5a15ba1d5fead4eca4ab6ed53d95d6a8807795f9
DARJIDASHRATH <110831773+DARJIDASHRATH@users.noreply.github.com>
2022-08-08 19:59:19 +0530

    Add files via upload

---
+++ Dog.java
@@ -0,0 +1,16 @@
+package thisandsuper;
+/**
+ * 
+ * @author DASHRATH
+ * @version 11.1
+ * @since
+ */
+public class Dog extends Animal {
+
+	Dog() {
+		String color = "Brown";
+		System.out.println("the Dog color is" + super.color);
+		System.out.println("the Dog color is" + color);
+	}
+
+}

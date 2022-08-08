7b2f138a344d7a1a23071cebefd141dffd81db86
DARJIDASHRATH <110831773+DARJIDASHRATH@users.noreply.github.com>
2022-08-08 19:49:09 +0530

    Add files via upload

--- src/polymorphismm/ComplieTime.java
+++ src/polymorphismm/ComplieTime.java
@@ -1,5 +1,10 @@
 package polymorphismm;
-
+/** Complittime perant class
+ * 
+ * @author DASHRATH
+ * @version 16.0.1
+ * @since
+ */
 public class ComplieTime {
 
 	static int schoolOne(int class1, int class2) {

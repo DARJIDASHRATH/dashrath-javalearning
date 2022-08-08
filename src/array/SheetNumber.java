793b0be34cccbcf2d13ba86090d9a6cd4a742954
DARJIDASHRATH <110831773+DARJIDASHRATH@users.noreply.github.com>
2022-08-08 19:26:27 +0530

    Add files via upload

--- src/array/SheetNumber.java
+++ src/array/SheetNumber.java
@@ -3,9 +3,7 @@ package array;
 public class SheetNumber {
 
 	public static void main(String[] args) {
-		
 		int[] conductor = {1,2,3,4,5,6,7,8,9,10};
-		
 		System.out.println(conductor[0]);
 	}
 

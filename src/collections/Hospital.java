48afe52af3bc1dacc2fd39347de260c6613890fc
DARJIDASHRATH <110831773+DARJIDASHRATH@users.noreply.github.com>
2022-08-09 18:32:37 +0530

    Add files via upload

---
+++ src/collections/Hospital.java
@@ -0,0 +1,32 @@
+package collections;
+
+/**
+ * Demostrate comperable interface
+ * 
+ * 
+ * @author DASHRATH
+ * @version 23.1.1
+ * @since
+ */
+class Hospital implements Comparable<Hospital> {
+
+	int roomNumber;
+	String name;
+	int date;
+
+	Hospital(int roomNumber, String name, int date) {
+		this.roomNumber = roomNumber;
+		this.name = name;
+		this.date = date;
+	}
+
+	public int compareTo(Hospital kd) {
+		if (date == kd.date)
+			return 1;
+		else if (date > kd.date)
+			return 0;
+		else
+			return 2;
+	}
+
+}

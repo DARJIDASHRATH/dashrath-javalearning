d97a6fcffc0179b144f2718054c9dbc1d8663a4e
DARJIDASHRATH <110831773+DARJIDASHRATH@users.noreply.github.com>
2022-08-09 12:53:04 +0530

    Add files via upload

---
+++ src/collections/RemoveDuplicateValue.java
@@ -0,0 +1,23 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicateValue {
+
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<>(Arrays.asList("A", "B", "C", "D", "A", "C", "E", "E", "E"));
+		System.out.println("List of Duplicate Value:" + " " + list);
+		// Convert ArrayList into Set
+		Set<String> newList = new LinkedHashSet<String>();
+		newList.addAll(list);
+		// Clearing list
+		newList.clear();
+		// Add Arraylist data in Set
+		newList.addAll(list);
+		System.out.println("Removed Duplicate value from list:" +" "+ newList);
+	}
+
+}

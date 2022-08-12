d4cb003f00fb07e3bd92a24c2751d72ef2070ba8
DARJIDASHRATH <110831773+DARJIDASHRATH@users.noreply.github.com>
2022-08-12 11:45:06 +0530

    Add files via upload

---
+++ src/mytestfile/ExceptionHandling.java
@@ -0,0 +1,26 @@
+package mytestfile;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ * Exception handling 
+ * @author DASHRATH
+ * @version 25.4
+ * @since
+ */
+
+public class ExceptionHandling {
+
+	public static void main(String[] args) throws IOException {
+		Scanner read = new Scanner(new File("myTestFile.txt"));
+		while (read.hasNext()) {
+			String w = read.next();
+			System.out.println(w);
+			close(read);
+		}
+	}
+
+	private static void close(Scanner read) {
+	}
+
+}

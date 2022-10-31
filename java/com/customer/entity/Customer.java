7c1abde60b06383c844390298ef54f932a0586bf
DARJIDASHRATH <110831773+DARJIDASHRATH@users.noreply.github.com>
2022-10-31 12:33:20 +0530

    Add files via upload

---
+++ java/com/customer/entity/Customer.java
@@ -0,0 +1,46 @@
+package com.customer.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+
+
+@Entity
+@Table(name = "customer")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(name = "first_name")
+	private String firstName;
+	@Column(name = "last_name")
+	private String lastName;
+	@Column(name = "date_of_birth")
+	private Date dateOfBirth;
+	@Column(unique=true, name = "mobile_number")
+	private String mobileNumber;
+	@Column(name = "address1")
+	private String address1;
+	@Column(name = "address2")
+	private String address2;
+	@Column(name = "age")
+	private Integer age;
+	@Column(name = "gender")
+	private String gender;
+	@Column(unique=true, name = "email_id")
+	private String email;
+	
+}

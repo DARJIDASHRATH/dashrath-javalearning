17d3dbc53a2c1a4352b72249b186a40eb6f19e91
DARJIDASHRATH <110831773+DARJIDASHRATH@users.noreply.github.com>
2022-11-02 12:23:33 +0530

    Add files via upload

---
+++ practical 28/java/com/customer/controller/CustomerController.java
@@ -0,0 +1,74 @@
+package com.customer.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.customer.service.CustomerService;
+import com.customer.model.CustomerModel;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+	
+	@Autowired
+	private CustomerService customerService;
+	
+	@GetMapping
+	public String listCustomers(Model model) {
+		List<CustomerModel> list = customerService.getAllCustomers();
+		model.addAttribute("customer", list);
+		return "customerlist";
+	}
+
+	@GetMapping("/new")
+	public String createCustomerForm(Model model) {
+		CustomerModel customer = new CustomerModel();
+		model.addAttribute("customer", customer);
+		return "home";
+	}
+
+	@PostMapping
+	public String saveCustomer(@ModelAttribute("customer") CustomerModel customer) {
+		customerService.saveCustomer(customer);
+		return "redirect:/customer";
+	}
+
+	@GetMapping("/new/{id}")
+	public String editCustomerForm(@PathVariable int id, Model model) {
+		model.addAttribute("customer", customerService.getCustomerById(id));
+		return "home";
+	}
+
+	@PostMapping("/{id}")
+	public String updateCustomer(@PathVariable int id, @ModelAttribute("customer") CustomerModel customer,
+			Model model) {
+		CustomerModel existingCustomer = customerService.getCustomerById(id);
+		existingCustomer.setId(id);
+		existingCustomer.setFirstName(customer.getFirstName());
+		existingCustomer.setLastName(customer.getLastName());
+		existingCustomer.setDateOfBirth(customer.getDateOfBirth());
+		existingCustomer.setMobileNumber(customer.getMobileNumber());
+		existingCustomer.setAddress1(customer.getAddress1());
+		existingCustomer.setAddress2(customer.getAddress2());
+		existingCustomer.setAge(customer.getAge());
+		existingCustomer.setGender(customer.getGender());
+		existingCustomer.setEmail(customer.getEmail());
+		customerService.updateCustomer(existingCustomer);
+		return "redirect:/customer";
+	}
+
+	@GetMapping("/{id}")
+	public String deleteCustomer(@PathVariable int id) {
+		customerService.deleteCustomerById(id);
+		return "redirect:/customer";
+	}
+
+}

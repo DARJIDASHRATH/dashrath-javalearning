82608781ea15997b9ab00fef02efc6413a030178
DARJIDASHRATH <110831773+DARJIDASHRATH@users.noreply.github.com>
2022-11-02 19:08:01 +0530

    Add files via upload

---
+++ mybatis/main/java/com/example/mybatis/springbootmybatis/mapper/EmployeeMapper.java
@@ -0,0 +1,20 @@
+package com.example.mybatis.springbootmybatis.mapper;
+
+import com.example.mybatis.springbootmybatis.model.Employee;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectKey;
+
+import java.util.List;
+
+@Mapper
+public interface EmployeeMapper {
+
+    @Select("select * from employees")
+    List<Employee> findAll();
+    @Insert("insert into employees(name,salary) values(#{name},#{salary})")
+    @SelectKey(statement = "SELECT LAST_INSERT_ID()", keyProperty = "id",
+            before = false, resultType = Integer.class)
+    void insert(Employee employee);
+}
